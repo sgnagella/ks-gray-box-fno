5fec6457c164ddd855172d5f85c573a507fb1475
sgnagella <sgnagella@ucsb.edu>
2024-06-24 22:13:29 -0700

    further training model

--- run_model.py
+++ run_model.py
@@ -19,6 +19,7 @@ def main():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     dirname = os.path.dirname(__file__)
+    file_model = "ks_model_v2.pth"; "ks_model.pth"
     pth_file = os.path.join(dirname, 'models', 'ks_model.pth')
     file = "ks_soln_ft_N_128_dt_0.25_tmax_500.pt"
     filename = os.path.join(dirname, 'training_data', file)
